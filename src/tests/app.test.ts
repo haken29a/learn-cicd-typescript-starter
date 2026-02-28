732d9d10d90c911b697a359ad7e7dd36ff7b4a5e
Richer Dinelle <haken29a@gmail.com>
2026-02-28 12:25:20 -0500

    Added test for ci that will fail

---
+++ src/tests/app.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth.js";
+import { IncomingHttpHeaders } from "http";
+
+let headers: IncomingHttpHeaders = {
+    'content-type': 'application/json',
+    'user-agent': 'My-App/1.0',
+    'accept': '*/*'
+};
+headers["authorization"] = "test";
+test("Testing to get API key", () => {
+    expect(getAPIKey(headers)).toBe("good")
+})
+
