97feb5372cc4d060162be9b2293ff66d94733a92
Richer Dinelle <haken29a@gmail.com>
2026-03-02 15:09:10 -0500

    Added prettier npm package

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -3,12 +3,11 @@ import { getAPIKey } from "../api/auth.js";
 import { IncomingHttpHeaders } from "http";
 
 let headers: IncomingHttpHeaders = {
-    'content-type': 'application/json',
-    'user-agent': 'My-App/1.0',
-    'accept': '*/*'
+  "content-type": "application/json",
+  "user-agent": "My-App/1.0",
+  accept: "*/*",
 };
 headers["authorization"] = "test";
 test("Testing to get API key", () => {
-    expect(getAPIKey(headers)).toBeNull
-})
-
+  expect(getAPIKey(headers)).toBeNull;
+});
